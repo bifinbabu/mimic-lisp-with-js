b646a22c39cde006d4719139fdf8a9259ca5dae3
bifinbabu <bifinbabu088@gmail.com>
2024-06-27 12:31:01 +0530

    Basic arithmetic operations implemeneted

--- compiler.js
+++ compiler.js
@@ -1,24 +1,45 @@
-const { add, add1 } = require("./programs");
+const {
+  add,
+  add1,
+  subtract,
+  multiply,
+  divide,
+  nested,
+  add2,
+  reminder,
+} = require("./programs");
 
-let first = (list) => (list?.length > 1 ? list[0] : undefined);
-let rest = (list) => (list?.length > 1 ? list.slice(1) : undefined);
+let first = (list) => (list?.length > 0 ? list[0] : undefined);
+let rest = (list) => (list?.length > 1 ? list.slice(1) : []);
 
 let interpret = function (list) {
+  if (!Array.isArray(list)) {
+    return list; // If it's not a list, return it as is (base case for recursion)
+  }
   let car = first(list);
-  let cdr = rest(list);
-  console.log("Executing", list, car, typeof cdr.slice(1));
+  let cdr = rest(list).map(interpret); // Recursively interpret each element in the rest of the list
+  console.log("first", cdr);
   switch (car) {
     case "+":
-      if (
-        typeof cdr.slice(1) == "object" &&
-        typeof cdr.slice(1)[0] == "object"
-      ) {
-        console.log("first", cdr.slice(1));
-        return interpret(cdr.slice(1)[0]);
-      } else {
-        return cdr.reduce((total, item) => total + item, 0);
-      }
+      return cdr.reduce((total, item) => total + item, 0);
+    case "-":
+      return cdr.reduce((total, item) => total - item);
+    case "*":
+      return cdr.reduce((total, item) => total * item, 1);
+    case "/":
+      return cdr.reduce((total, item) => total / item);
+    case "%":
+      return cdr.reduce((total, item) => total % item);
+    default:
+      throw new Error("Unknown operation: " + car);
   }
 };
 
-console.log(interpret(add1));
+// console.log(interpret(add)); // Output: 5
+console.log(interpret(add1)); // Output: 6
+console.log(interpret(add2));
+// console.log(interpret(subtract)); // Output: 3
+// console.log(interpret(multiply)); // Output: 6
+// console.log(interpret(divide)); // Output: 3
+// console.log(interpret(nested)); // Output: 26
+console.log(interpret(reminder));
--- programs.js
+++ programs.js
@@ -1,7 +1,19 @@
 let add = ["+", 2, 3];
 let add1 = ["+", 2, ["+", 1, 3]];
+let add2 = ["+", 2, 3, ["+", 1, 3]];
+let subtract = ["-", 5, 2];
+let multiply = ["*", 2, 3];
+let divide = ["/", 6, 2];
+let nested = ["+", 2, ["*", 3, ["-", 10, 2]]];
+let reminder = ["%", 10, 3];
 
 module.exports = {
   add,
   add1,
+  add2,
+  subtract,
+  multiply,
+  divide,
+  nested,
+  reminder,
 };
